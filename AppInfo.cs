fa41e86d250dcea3f1bf9874617d08b9b6a42865
kejifrs <kejifrs@gmail.com>
2026-04-01 17:40:07 +0800

    优化显示

--- AppInfo.cs
+++ AppInfo.cs
@@ -4,14 +4,24 @@ namespace DmPayQuery;
 
 public static class AppInfo
 {
+    private static readonly Assembly s_assembly = Assembly.GetExecutingAssembly();
+
     public static string ProductName { get; } =
-        Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyProductAttribute>()?.Product
+        s_assembly.GetCustomAttribute<AssemblyProductAttribute>()?.Product
         ?? "DmPayQuery";
 
     public static string Version { get; } =
-        Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
-        ?? Assembly.GetExecutingAssembly().GetName().Version?.ToString()
-        ?? "1.0.0";
+        GetDisplayVersion();
 
     public static string MainWindowTitle => $"{ProductName} v{Version}";
+
+    private static string GetDisplayVersion()
+    {
+        var informationalVersion = s_assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        if (!string.IsNullOrWhiteSpace(informationalVersion))
+            return informationalVersion.Split('+')[0];
+
+        return s_assembly.GetName().Version?.ToString()
+        ?? "1.0.0";
+    }
 }
